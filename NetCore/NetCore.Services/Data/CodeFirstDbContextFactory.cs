b0d997397531e5d82d37bb4947bf7051cbb0cd99
Jeong-soo, Kim <programmingsquare@gmail.com>
2021-11-29 15:23:21 +0900

    Update CodeFirstDbContextFactory.cs
    _configPath comment  추가

--- NetCore/NetCore.Services/Data/CodeFirstDbContextFactory.cs
+++ NetCore/NetCore.Services/Data/CodeFirstDbContextFactory.cs
@@ -8,6 +8,11 @@ namespace NetCore.Services.Data
     // Microsoft.EntityFrameworkCore.Design NuGet Package 2.1.2
     public class CodeFirstDbContextFactory : IDesignTimeDbContextFactory<CodeFirstDbContext>
     {
+        /*
+         * 조금 불편해도 appsettings.json을 Web Project에서만
+         * 관리하도록 하기 위해 수동으로 파일경로를 지정합니다.
+         * 수강생 여러분들은 각자의 웹프로젝트 위치로 변경하시면 됩니다.
+         */
         private const string _configPath = @"D:\Repository\pgmsquare\NetCore\NetCore\NetCore.Web.V5\appsettings.json";
 
         public CodeFirstDbContext CreateDbContext(string[] args)
